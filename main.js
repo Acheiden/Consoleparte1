681d0f75b95b8fe5b4b0951531ac52455300208a
Acheiden <152915026+Acheiden@users.noreply.github.com>
2024-02-14 14:40:29 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+ let nome = "Vinicius"
+
+ let sobrenome = "Heiden"
+
+ let nomecompleto = nome.concat(sobrenome)
+ console.log(nomecompleto);
+
+ console.log(nomecompleto.length);
+
+ let irma = nomecompleto.replace("Vinicius" , "Isabela") 
+ console.log(irma);
+
+ console.log(irma.toUpperCase());
+
+ let a = 15;
+
+ let b = 15;
+
+ soma = a + b;
+ console.log(soma);
+
+subtracao = a - b;
+ console.log(subtracao);
+
+ divisao = a / b;
+ console.log( divisao);
+
+mult = a * b;
+ console.log(mult);
+
+ raizquadrada = Math.sqrt(a);
+ console.log(raizquadrada);
